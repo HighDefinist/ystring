f14a389b41cbdce7dd59e17a8cb9c71117b7aa8d
Martin Zintl <martinzintl@gmail.com>
2018-03-01 21:54:50 +0100

    DIfferent folder structure; One test

---
+++ test/basic/ystr_test.cpp
@@ -0,0 +1,33 @@
+#include "mz/ystring.h"
+#include "mz/time.h"
+ 
+using namespace std;
+using namespace std::mz;
+
+#define CATCH_CONFIG_MAIN  
+#include "catch.hpp"
+
+TEST_CASE("ystring main", "Main functionality") {
+  SECTION("ystr") {
+    REQUIRE(ystr("A=% B=% C=% D=% E=% F=%\n", 1, 2.3, "abbl", 1.23456789012345678901, -12, make_tuple(1, make_tuple(2, 3.0))) == "A=1 B=2.3 C=abbl D=1.23456789012346 E=-12 F=[1;[2;3]]\n");
+  }
+}
+
+//
+//int main(){
+//   
+//  Tbench b;
+//  for(int j=0; j<100; j++) b.Run([&]{
+//    for(int i=0; i<1e3; i++){
+//      auto v1 = ystr("A=% B=% C=% D=% E=% F=%\n", 1, 2.3, "abbl", 1.23456789012345678901, -12, make_tuple(1, make_tuple(2, 3.0)));
+//    }
+//  }); 
+//
+//
+//  yprintf("% ns per string parsing:\n",b.dur_avg()*1000000);
+//  yprintf("A=% B=% C=% D=% E=% F=%\n",1,2.3,"abbl", 1.23456789012345678901,-12,make_tuple(1,make_tuple(2,3.0)));
+//  yprintf("___\n");
+//  getchar();
+//  return 0;
+//}
+//
