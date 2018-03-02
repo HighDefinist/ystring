2b5fbeef859e5557a757bae7e4a4b3815433e102
Martin Zintl <martinzintl@gmail.com>
2018-03-02 16:15:10 +0100

    More tests

--- test/basic/ystr_test.cpp
+++ test/basic/ystr_test.cpp
@@ -8,8 +8,25 @@ using namespace std::mz;
 #include "catch.hpp"
 
 TEST_CASE("ystring main", "Main functionality") {
-  SECTION("ystr") {
-    REQUIRE(ystr("A=% B=% C=% D=% E=% F=%\n", 1, 2.3, "abbl", 1.23456789012345678901, -12, make_tuple(1, make_tuple(2, 3.0))) == "A=1 B=2.3 C=abbl D=1.23456789012346 E=-12 F=[1;[2;3]]\n");
+  SECTION("Conversion basics") {
+    REQUIRE(ystr("Integer=%", 2) == "Integer=2");
+    REQUIRE(ystr("Float=%", 3.5f) == "Float=3.5");
+    REQUIRE(ystr("Double=%", 4.5) == "Double=4.5");
+    REQUIRE(ystr("String=%", "abc") == "String=abc");
+    REQUIRE(ystr("WString=%", L"abc") == "WString=abc");
+    REQUIRE(ystr("Tuple=%",make_tuple(1, 2, "abc")) == "Tuple=[1;2;abc]");
+    REQUIRE(ystr("PercentSign=%%") == "PercentSign=%");
+    REQUIRE(ystr("NoParameter") == "NoParameter");
+  }
+  SECTION("Special stuff"){
+    REQUIRE_THROWS(ystr("MissingArgument=%"));
+    REQUIRE_THROWS(ystr("TooManyArguments", 3));
+    REQUIRE(ystr("SpecialCase=%%%", 2) == "SpecialCase=%2");
+    REQUIRE_THROWS(ystr("SpecialCase=%%%"));
+    REQUIRE_THROWS(ystr("SpecialCase=%%%",2,3));
+    REQUIRE(ystr("SpecialCase=%%%%") == "SpecialCase=%%");
+    REQUIRE(ystr("SpecialCase=% %%",2) == "SpecialCase=2 %");
+    REQUIRE(_str(23.2) == "23.2");
   }
 }
 
